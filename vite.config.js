f08b3e121ecf8922df7af6f6442cdebe504f58eb
Ivan Alvarez <ivalvarez22@gmail.com>
2025-02-26 19:51:12 -0400

    update PWA configuration, enhance favicon support, and modify logo; adjust styles for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -9,10 +10,65 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+
+      manifest: {
+        name: 'Notidy',
+
+        short_name: 'Notidy',
+
+        description: 'A PWA example for notification',
+
+        theme_color: '#1ba13f',
+
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+
+            sizes: '64x64',
+
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+
+            sizes: '192x192',
+
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+
+            sizes: '512x512',
+
+            type: 'image/png',
+
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+
+            sizes: '512x512',
+
+            type: 'image/png',
+
+            purpose: 'maskable',
+          },
+        ],
+      },
+
+      // devOptions: {
+      //   enabled: true,
+      // },
+    }),
   ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
