ca0c8af3f734862feeb2a8f5dac708fb58bdbefd
Ivan Alvarez <ivalvarez22@gmail.com>
2025-02-25 21:57:48 -0400

    update navigation link from "Install" to "About"

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Notify</RouterLink>
-        <RouterLink to="/about">Install</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
